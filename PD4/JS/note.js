e9a6b6b514fa62bb06d659d295343244a56a4c19
Kristupas25 <capledvin@gmail.com>
2023-10-10 13:00:17 +0300

    asdsa

---
+++ PD4/JS/note.js
@@ -0,0 +1,13 @@
+let petName;
+let petAge;
+
+petName = "Rex";
+petAge = 3;
+
+console.log("Pet name:", petName);
+console.log("Pet age:", petAge);
+
+petName = "Tom";
+petAge = 5;
+
+const buildingAddress = "123 Main Street";
